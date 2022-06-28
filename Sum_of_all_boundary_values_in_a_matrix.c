163b069b60f715092c0bb8df31fe09cd64841d32
21A91A05E1 <97424503+21A91A05E1@users.noreply.github.com>
2022-06-28 22:42:04 +0530

    #include<stdio.h> int main() {     int i,j,arr[100][100],n,m,sum=0;     scanf("%d%d",&n,&m);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             if(i==0 || i==n-1 || j==0 || j==m-1)             sum=sum+arr[i][j];         }     }     printf("%d",sum); }

--- Sum_of_all_boundary_values_in_a_matrix.c
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -14,7 +14,7 @@ int main()
     {
         for(j=0;j<m;j++)
         {
-            if(i==0 || i==n-1 || j==0 || j==n-1)
+            if(i==0 || i==n-1 || j==0 || j==m-1)
             sum=sum+arr[i][j];
         }
     }
