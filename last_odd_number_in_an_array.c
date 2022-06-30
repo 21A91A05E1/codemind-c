cde466a6350c9d3c65b5fc19e34653e711d543c3
21A91A05E1 <97424503+21A91A05E1@users.noreply.github.com>
2022-06-30 10:26:35 -0700

    #include<stdio.h> int main() {     int arr[100],n,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=n-1;i>=0;i--)     {         if(arr[i]%2!=0)         {             printf("%d",arr[i]);             break;         }     } }

--- last_odd_number_in_an_array.c
+++ last_odd_number_in_an_array.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int arr[100],i,n;
+    int arr[100],n,i;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
@@ -9,7 +9,7 @@ int main()
     }
     for(i=n-1;i>=0;i--)
     {
-        if(arr[i]%2)
+        if(arr[i]%2!=0)
         {
             printf("%d",arr[i]);
             break;
